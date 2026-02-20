b09139caec4d412fdc7e998c3913674adc7bbc25
naszczep <natalia.diana.szczepanek@cern.ch>
2026-02-20 14:34:23 +0100

    Added metadata and seo

--- app.py
+++ app.py
@@ -76,6 +76,48 @@ async def favicon():
     return Response(status_code=204)
 
 
+@app.get("/robots.txt")
+async def robots_txt():
+    """Serve robots.txt for search engine crawlers"""
+    content = """User-agent: *
+Allow: /
+Disallow: /admin
+Disallow: /api/import/
+Sitemap: https://computespecsdb.com/sitemap.xml
+"""
+    return Response(content=content, media_type="text/plain")
+
+
+@app.get("/sitemap.xml")
+async def sitemap_xml():
+    """Serve sitemap.xml for search engine indexing"""
+    content = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>https://computespecsdb.com/</loc>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+    <url>
+        <loc>https://computespecsdb.com/visualizations</loc>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>https://computespecsdb.com/api</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
+    </url>
+    <url>
+        <loc>https://computespecsdb.com/docs</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
+    </url>
+</urlset>
+"""
+    return Response(content=content, media_type="application/xml")
+
+
 class CPUSpecResponse(BaseModel):
     """Response model for compute specifications"""
     id: int
